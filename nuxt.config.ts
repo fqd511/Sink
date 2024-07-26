8127813fe0ffa34708572fe2cb8bdbd72e625652
v <15816056+fqd511@users.noreply.github.com>
2024-07-26 19:03:59 +0800

    feat: update header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -26,9 +26,10 @@ export default defineNuxtConfig({
     },
     '/api/link/create': {
       headers: {
+        // 'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Origin': 'localhost, *.9jian.tech',
         'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        'Access-Control-Allow-Methods': 'POST, OPTIONS',
+        'Access-Control-Allow-Methods': '*',
         'Access-Control-Allow-Credentials': 'true',
       },
     },
